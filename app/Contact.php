59b0a5b19d06fe379fc504653cb3b6c42ffb4218
Marcelo Bohn <marcelo.bohn@gmail.com>
2020-03-18 10:50:42 -0300

    Fix model Contact

--- app/Contact.php
+++ app/Contact.php
@@ -6,24 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    public function up() 
-    { 
-        Schema::create('contacts', function (Blueprint $table) { 
-            $table->increments('id'); 
-            $table->string('name'); 
-            $table->string('contact'); 
-            $table->string('email'); 
-            $table->timestamps(); 
-        }); 
-    } 
-   
-    /** 
-     * Reverse the migrations. 
-     * 
-     * @return void 
-     */ 
-    public function down() 
-    { 
-        Schema::dropIfExists('contacts'); 
-    } 
+    protected $fillable = [
+        'name',
+        'contact',
+        'email',
+       ];
 }
